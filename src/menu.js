30ca568e4dfe73311199e42c47942c63f1107615
Casper Gamborg <Gamborgc@gmail.com>
2025-05-09 10:14:03 +0200

    food pictures are now imported

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,8 @@
-// console.log("HI from Menu");
+import caesar from "./assets/caesar.jpg";
+import carbo from "./assets/carbo.jpg";
+import lava from "./assets/lava.jpg";
+import pizza from "./assets/pizza.jpg";
+import salmon from "./assets/salmon.jpeg";
 
 export function menuPageRender() {
   const menuItems = [
@@ -7,39 +11,67 @@ export function menuPageRender() {
       description:
         "Classic pizza with tomato sauce, mozzarella, and fresh basil.",
       price: 9.99,
+      img: pizza,
     },
     {
       name: "Spaghetti Carbonara",
       description: "Pasta with creamy egg sauce, pancetta, and parmesan.",
       price: 12.5,
+      img: carbo,
     },
     {
       name: "Caesar Salad",
       description: "Romaine lettuce, croutons, parmesan, and Caesar dressing.",
       price: 7.25,
+      img: caesar,
     },
     {
       name: "Grilled Salmon",
       description:
         "Salmon fillet with lemon butter sauce and seasonal vegetables.",
       price: 15.75,
+      img: salmon,
     },
     {
       name: "Chocolate Lava Cake",
       description:
         "Warm chocolate cake with a gooey center, served with vanilla ice cream.",
       price: 6.5,
+      img: lava,
     },
   ];
 
-  /* TO DO: Gør således, at vi har 3 p-elementer, og indsæt name, description og price i hver deres, samt placer alle 3 inde i deres individuelle div */
   function createFoodElements() {
     const foodContainer = document.createElement("div");
-    menuItems.forEach(({ name, description, price }) => {
+    menuItems.forEach(({ name, description, price, img }) => {
       foodContainer.className = "foodContainer";
-      const foodItems = document.createElement("p");
-      foodItems.innerText = `${name}, ${description} - ${price}`;
-      foodContainer.appendChild(foodItems);
+      const foodItemsDiv = document.createElement("div");
+      foodItemsDiv.classList = "foodItems";
+
+      const foodName = document.createElement("p");
+      foodName.className = "foodName";
+      foodName.innerText = name;
+
+      const foodDescription = document.createElement("p");
+      foodDescription.className = "foodDescription";
+      foodDescription.innerText = description;
+
+      const foodPrice = document.createElement("p");
+      foodPrice.className = "foodPrice";
+      foodPrice.innerText = price;
+
+      const imgContainer = document.createElement("img");
+      imgContainer.src = img;
+      imgContainer.class = "image";
+
+      console.log(imgContainer);
+
+      foodItemsDiv.appendChild(foodName);
+      foodItemsDiv.appendChild(foodDescription);
+      foodItemsDiv.appendChild(foodPrice);
+      foodItemsDiv.appendChild(imgContainer);
+
+      foodContainer.appendChild(foodItemsDiv);
     });
     return foodContainer;
   }
